002d8d5c37131b1f3d561082fffcf227b3cca70c
Egono <henriksenegon2@gmail.com>
2023-08-14 12:01:06 +0200

    Fortsatte på main och började på GamePanel.java

---
+++ src/main/GamePanel.java
@@ -0,0 +1,10 @@
+package main;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+    
+    //Skärminställningar
+    final int originalTileSize = 16; //16x16 texturer som standard
+    final int scale = 3; //skala upp dem så det inte ser så litet ut på skärmen
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,17 @@
 package main;
 
+import javax.swing.JFrame;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Platformer");
+
+        window.setLocationRelativeTo(null); //ospecificerad window location = mitten av skärmen.
+        window.setVisible(true);
     }
 }
