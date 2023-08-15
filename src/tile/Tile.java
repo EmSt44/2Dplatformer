68e72e3013176fe1491e9f9149e71053628a1da3
Emil Strand <cheapsheep39@gmail.com>
2023-08-15 10:59:12 +0200

    added initial tile files, not functional

---
+++ src/tile/Tile.java
@@ -0,0 +1,8 @@
+package tile;
+
+import java.awt.image.BufferedImage;
+
+public class Tile {
+public BufferedImage image; //ska ha en textur
+public Boolean collision = false; //ska kunna hantera kollisioner med objekt
+} 
