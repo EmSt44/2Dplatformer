722ec408642de64f1f126286ffa057a12880d1a8
SimFra02 <simon@franzens.org>
2023-08-22 13:21:56 +0200

    object system added

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -10,6 +10,7 @@ import javax.swing.JPanel;
 
 import entity.*;
 import tile.TileManager;
+import object.*;
 
 public class GamePanel extends JPanel implements Runnable{
     
@@ -37,6 +38,7 @@ public class GamePanel extends JPanel implements Runnable{
     //Entity, objekt
     public Player player = new Player(this, keyH);
     public Entity npc[] = new Entity[10]; //maximal mängd olika NPC. Öka siffran för att ändra.
+    public SuperObject obj[] = new SuperObject[10]; //maximal mängd olika Objekt du kan ha på mappen.
 
     //TileManager, KeyHandler, liknande managers
     TileManager tileM = new TileManager(this);
@@ -75,6 +77,8 @@ public class GamePanel extends JPanel implements Runnable{
     //Väljer initiell gameState
     public void setupGame() {
         gameState = menuState;
+
+        aSetter.setObject();
     }
 
     public void startGameThread() {
@@ -115,7 +119,7 @@ public class GamePanel extends JPanel implements Runnable{
 
             //FPS counter
             if(timer >= 1000000000) {
-                System.out.println("FPS: " + drawCount);
+                //System.out.println("FPS: " + drawCount);
                 drawCount = 0;
                 timer = 0;
             }
@@ -152,6 +156,13 @@ public class GamePanel extends JPanel implements Runnable{
         else {
             // Ritar TILES, viktigt: draw tiles innan player!
             tileM.draw(g2); 
+
+            // Ritar Objekt
+            for(int i = 0; i < obj.length; i++) {
+                if(obj[i] != null) {
+                    obj[i].draw(g2, this);
+                }
+            }
 
             // Ritar PLAYER
             player.draw(g2);
