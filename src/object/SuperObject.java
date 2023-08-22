ef9bcc0c679533197adb463b172dddb8e84f5df8
Emil Strand <cheapsheep39@gmail.com>
2023-08-22 11:24:09 +0200

    lade till SuperObject och OBJ_heart

---
+++ src/object/SuperObject.java
@@ -0,0 +1,12 @@
+package object;
+
+import java.awt.image.BufferedImage;
+
+public class SuperObject {
+
+    public BufferedImage image, image2, image3;
+    public String name;
+    public Boolean collision = false;
+    public int worldX, worldY;
+    
+}
